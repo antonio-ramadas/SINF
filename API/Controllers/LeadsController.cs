be622c797c297f43415aade746bc59937251f1af
guilhermevpinto <guilhermevpinto@gmail.com>
2016-11-23 23:55:24 +0000

    LeadsController

--- API/Controllers/LeadsController.cs
+++ API/Controllers/LeadsController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace SFA_REST.Controllers
 {
-    public class NeedsController : ApiController
+    public class LeadsController : ApiController
     {
         
     }
