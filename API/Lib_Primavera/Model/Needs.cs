62a8a1f92748473e88891ff2f9aa9bdf5715bf72
guilhermevpinto <guilhermevpinto@gmail.com>
2016-11-23 03:55:17 +0000

    Needs class

---
+++ API/Lib_Primavera/Model/Needs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SFA_REST.Lib_Primavera.Model
+{
+    public class Needs
+    {
+        public string id { get; set; }
+
+        public string customerID { get; set; }
+
+        public string productID { get; set; }
+
+        public string quantity { get; set; }
+    }
+}
