0ea0a6190985b55a426ecc2e5c223b190eeb3330
guilhermevpinto <guilhermevpinto@gmail.com>
2016-12-14 10:12:11 +0000

    llllk

---
+++ API/Lib_Primavera/Model/SalesOrder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SFA_REST.Lib_Primavera.Model
+{
+    public class SalesOrder
+    {
+
+        public string id
+        {
+            get;
+            set;
+        }
+
+        public string entity
+        {
+            get;
+            set;
+        }
+
+        public string address
+        {
+            get;
+            set;
+        }
+
+        public int numDoc
+        {
+            get;
+            set;
+        }
+
+        public DateTime date
+        {
+            get;
+            set;
+        }
+
+        public double totalMerc
+        {
+            get;
+            set;
+        }
+
+        public string serie
+        {
+            get;
+            set;
+        }
+
+        public string salesRep
+        {
+            get;
+            set;
+        }
+
+        public List<Model.LinhaDocVenda> LinhasDoc
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
