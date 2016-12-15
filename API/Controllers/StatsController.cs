d7fedfa2f8591e66d3b32ce1bb323daa7b0475ab
guilhermevpinto <guilhermevpinto@gmail.com>
2016-12-15 05:38:27 +0000

    stats of the year

--- API/Controllers/StatsController.cs
+++ API/Controllers/StatsController.cs
@@ -127,11 +127,17 @@ namespace SFA_REST.Controllers
         /// <returns> List of Top SalesRepresentative </returns>
         [Route("api/stats/salesRep/{number}")]
         [HttpGet]
-        public IEnumerable<Lib_Primavera.Model.Stats.TopCategory> GetTopCategories(string number)
+        public IEnumerable<Lib_Primavera.Model.SalesRepresentative> GetTopSalesRep(string number)
         {
-            return Lib_Primavera.PriIntegration.GetTopSalesRep(id);
+            return Lib_Primavera.PriIntegration.GetTopSalesRep(number);
         }
 
+        /// <summary>
+        ///   GET method for the the profits of a year
+        /// </summary>
+        /// <returns> Money made in a year </returns>
+        [Route("api/stats/year/{year}")]
+        [HttpGet]
         public int getYearTotalMerc(string year)
         {
             return Lib_Primavera.PriIntegration.getYearTotalMerc(year);
